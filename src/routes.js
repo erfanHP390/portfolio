36266600ca5490e9ea654af0d0d7afde85a8e8c9
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-04 09:30:41 +0330

    create page resume

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import Index from "./Pages/Index/Index"
+import Home from "./Pages/Home/Home"
+import About from "./Pages/About/About"
+import Resume from "./Pages/Resume/Resume"
+import Works from "./Pages/Works/Works"
+import Contact from "./Pages/Contact/Contact"
+
+const routes = [
+    {path: "" , element: <Index />},
+    {path: "/" , element: <Home />},
+    {path: "/about" , element: <About />},
+    {path: "/resume" , element: <Resume />},
+    {path: "/works" , element: <Works />},
+    {path: "/contact" , element: <Contact />},
+]
+
+
+export default routes
