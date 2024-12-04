19fe72e5589ad15544d128ce4be4247e9dcdb0dd
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-04 23:21:04 +0330

    create home with styles

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import "./Home.css"
 import Header from '../../Components/Header/Header'
+import SocialBox from '../../Components/SocialBox/SocialBox';
 
+// import icons 
+import { FaGithub } from "react-icons/fa6";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaTelegramPlane } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -9,6 +15,21 @@ export default function Home() {
       <div className='z-50'>
         <div className='container'>
           <Header />
+          <div className="home-container">
+            <img src="./images/profile/profile.jpg" alt="" className='img-profile' />
+            <h3 className="title-profile">
+              عرفان حسینی
+            </h3>
+            <p className="subtitle-profile">برنامه نویس فرانت اند</p>
+            <div className="social-profile">
+              <SocialBox href={""}  icon={<FaGithub className='github' />} />
+              <SocialBox href={""}  icon={<RiInstagramFill className='instagram' />} />
+              <SocialBox href={""}  icon={<FaTelegramPlane className='telegram' />} />
+              <SocialBox href={""}  icon={<BsLinkedin className='linkedin' />} />
+
+            </div>
+            
+          </div>
         </div>
       </div>
     </div>
