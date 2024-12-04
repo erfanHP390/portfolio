9e90388ce7e1da9c90b863607c81d2d2667ad120
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-04 21:53:18 +0330

    delete route

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,3 @@
-import Index from "./Pages/Index/Index"
 import Home from "./Pages/Home/Home"
 import About from "./Pages/About/About"
 import Resume from "./Pages/Resume/Resume"
@@ -6,7 +5,6 @@ import Works from "./Pages/Works/Works"
 import Contact from "./Pages/Contact/Contact"
 
 const routes = [
-    {path: "" , element: <Index />},
     {path: "/" , element: <Home />},
     {path: "/about" , element: <About />},
     {path: "/resume" , element: <Resume />},
