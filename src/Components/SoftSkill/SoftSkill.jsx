4364fb85c298e702ca61d3c4a8766f7b6cbb49a4
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-05 23:06:44 +0330

    create component soft-skill with styles

---
+++ src/Components/SoftSkill/SoftSkill.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./SoftSkill.css"
+
+export default function SoftSkill({title}) {
+  return (
+    <button className='btn-resume'>
+      {title}
+    </button>
+  )
+}
